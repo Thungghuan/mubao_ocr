c62ec729d09df790353a8831b08ef9f1f4bf9b18
Thungghuan <1092887426@qq.com>
2022-11-03 17:51:41 +0800

    fix: error condition

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def ocr_image(image_url, session):
         for line in result:
             text_content += line[-1][0] + "\n"
 
-    if not session["content"]:
+    if "content" not in session or len(session["content"]) == 0:
         session["content"] = [text_content]
     else:
         session["content"].append(text_content)
@@ -44,7 +44,7 @@ def img_handler(message, session):
 
 @robot.text
 def text_handler(_, session):
-    if not session["content"] or len(session["content"]) == 0:
+    if "content" not in session or len(session["content"]) == 0:
         return "图片还没有处理完成哦"
     else:
         return "\n\n".join(session["content"])
