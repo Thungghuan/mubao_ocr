d1b6188254afafb007968e131bc8877dbeb45536
Thungghuan <1092887426@qq.com>
2022-11-03 15:18:21 +0800

    feat: add ocr handler

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from configparser import ConfigParser
+import tempfile
 import werobot
+import requests
+from paddleocr import PaddleOCR
 
 config = ConfigParser()
 config.read("config.ini")
@@ -10,9 +13,27 @@ token = config["werobot"]["token"]
 robot = werobot.WeRoBot(token)
 
 
-@robot.handler
-def hello(message):
-    return "Hello World!"
+def ocr_image(image_url):
+    image = requests.get(image_url).content
+    ocr = PaddleOCR(lang="ch")
+
+    text_content = ""
+
+    with tempfile.NamedTemporaryFile(mode="wb") as f:
+        f.write(image)
+        result = ocr.ocr(f.name)
+
+        for idx in range(len(result)):
+            res = result[idx]
+            for line in res:
+                print(line)
+
+
+# @robot.image 修饰的 Handler 只处理图片消息
+@robot.image
+def img(message):
+    ocr_image(message.img)
+    return message.img
 
 
 robot.config["HOST"] = config["werobot"]["host"]
